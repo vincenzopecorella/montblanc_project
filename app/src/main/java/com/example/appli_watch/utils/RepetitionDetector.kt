c25c1df709b6d5bd6b850262f4a4683d5877cf5f
yasmine <yasmine.elgoumi@gmail.com>
2022-12-02 19:46:17 +0100

    Squats new sample

--- app/src/main/java/com/example/appli_watch/utils/RepetitionDetector.kt
+++ app/src/main/java/com/example/appli_watch/utils/RepetitionDetector.kt
@@ -146,7 +146,7 @@ class RepetitionDetector(private val exercise: Exercise) {
 
                 if ((!waiting) && (corr_mix(integralDataAll,sampleIntegAll, indexCorrAll,dimension) > TRESHOLD_CORREl)) { //correlation performed + result compared to treshold value
                     //if threshold is exceeded, squat number is incremented and detection is deactivated for inBetweenSquatsWaitTime
-                    maxCorrPerRep = 0.0
+                    //maxCorrPerRep = 0.0
                     numberOfRepetitions += 1
                     waiting = true
                     waitCountdown = COUNTDOWN
